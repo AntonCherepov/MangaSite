60db447e8ecdb5633a4fa5a552c2a487f202cf7f
Anton Cherepov <anton.cherepov.97@gmail.com>
2019-03-03 16:48:29 +0300

    models v0.0.2

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,16 +1,33 @@
-from django.db import models
+from django.db.models import (ImageField, DateTimeField,
+                              TextField, FileField,
+                              CharField, ForeignKey,
+                              Model, CASCADE)
 
 
-class Main(models.Model):
-    """Модель для новостей"""
-    text = models.TextField('Text', max_length=500)
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
-    main_image = models.ImageField('Image_name')
+class User(Model):
+    """ Модель для пользователей"""
+
+    nick_name = CharField(max_length=50)
+    email = CharField(max_length=126)
+    password = TextField(max_length=254)
 
 
-class User(models.Model):
-    pass
+class Main(Model):
+    """ Модель хранения новости """
+
+    user = ForeignKey(User, verbose_name="User", on_delete=CASCADE)
+    headline = CharField('Headline', max_length=150)
+    text = TextField('Text', max_length=500)
+    pub_date = DateTimeField('date published', auto_now_add=True)
+    main_image = ImageField('Image_name')
+
+    def __str__(self):
+        return "{}".format(self.user)
 
 
-class Files(models.Model):
-    pass
+class Files(Model):
+    """ Модель хранения пути к файлу """
+
+    news = ForeignKey(Main, on_delete=CASCADE)
+    file_name = CharField('File name', max_length=200)
+    file_path = FileField(max_length=200)
