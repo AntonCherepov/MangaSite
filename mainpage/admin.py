f521224eec2b4e98f1db3a4aaff0c565245dfc05
Anton Cherepov <anton.cherepov.97@gmail.com>
2019-03-03 18:52:47 +0300

    models v0.0.3 + admin change

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from mainpage.models import File, User, Main
 
-# Register your models here.
+
+class MainAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pub_date', 'user', 'headline', 'text', 'main_image')
+
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'file_name', 'file_path', 'connector_to_news')
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nick_name', 'email', 'password')
+
+
+admin.site.register(Main, MainAdmin)
+admin.site.register(File, FileAdmin)
+admin.site.register(User, UserAdmin)
--- mainpage/models.py
+++ mainpage/models.py
@@ -1,21 +1,25 @@
 from django.db.models import (ImageField, DateTimeField,
                               TextField, FileField,
                               CharField, ForeignKey,
-                              Model, CASCADE)
+                              IntegerField, CASCADE,
+                              Model)
 
 
 class User(Model):
-    """ Модель для пользователей"""
+    """ Модель для пользователей """
 
     nick_name = CharField(max_length=50)
     email = CharField(max_length=126)
     password = TextField(max_length=254)
 
+    def __str__(self):
+        return "{}".format(self.nick_name)
+
 
 class Main(Model):
     """ Модель хранения новости """
 
-    user = ForeignKey(User, verbose_name="User", on_delete=CASCADE)
+    user = ForeignKey(User, verbose_name='User', on_delete=CASCADE)
     headline = CharField('Headline', max_length=150)
     text = TextField('Text', max_length=500)
     pub_date = DateTimeField('date published', auto_now_add=True)
@@ -25,9 +29,9 @@ class Main(Model):
         return "{}".format(self.user)
 
 
-class Files(Model):
+class File(Model):
     """ Модель хранения пути к файлу """
 
-    news = ForeignKey(Main, on_delete=CASCADE)
+    connector_to_news = IntegerField(default=0, blank=True)
     file_name = CharField('File name', max_length=200)
     file_path = FileField(max_length=200)
