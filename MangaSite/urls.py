a1cec046570eedde8af281f815b523ea3f5111b6
Anton Cherepov <anton.cherepov.97@gmail.com>
2019-03-31 20:27:17 +0300

    v0.3.1 (pre-user, added style for paginator buttons)

--- MangaSite/urls.py
+++ MangaSite/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico',
+         RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
     path('admin/', admin.site.urls),
     path('', include('mainpage.urls')),
 ]
